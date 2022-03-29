4748e29ed810a913779958230be48bc0b4a3cc88
Evgeniy Talko <etalko@gmail.com>
2022-03-29 22:35:27 +0300

    feat(api): appointments CRUD operations and hooks

--- src/shared/api/appointments.ts
+++ src/shared/api/appointments.ts
@@ -1,1 +1,81 @@
-export const test = ''
+import { MutationOptions, useMutation, useQueryClient } from 'react-query'
+
+import { api, useQuery, SubjectIdParam, Appointment } from 'shared/api'
+
+import { NanoId } from './types'
+
+// Фетчеры данных
+async function fetchAppointments() {
+    return await api.get('/appointments')
+}
+
+async function fetchAppointmentById(id: SubjectIdParam) {
+    return await api.get(`/appointments/${id}`)
+}
+
+// Хуки для использования данных на основе react-query
+function useAppointments() {
+    return useQuery(['appointments', 'all'], fetchAppointments)
+}
+
+function useAppointment(id: SubjectIdParam) {
+    return useQuery(['appointments', id], () => fetchAppointmentById(id))
+}
+
+function useAppointmentCreate(
+    newAppointment: Appointment,
+    options?: MutationOptions
+) {
+    const queryClient = useQueryClient()
+    return useMutation(() => api.post('/appointments', newAppointment), {
+        onSuccess: () => {
+            // Обновляем данные после успешного создания нового объекта
+            queryClient.invalidateQueries(['appointments', 'all'])
+        },
+        ...options,
+    })
+}
+
+function useAppointmentUpdate(
+    newAppointment: Appointment,
+    options?: MutationOptions
+) {
+    const queryClient = useQueryClient()
+
+    return useMutation(
+        () =>
+            api.put(`/appointments/${newAppointment.id}`, {
+                ...newAppointment,
+            }),
+        {
+            onSuccess: () => {
+                // Обновляем данные после успешного обновления объекта
+                queryClient.invalidateQueries([
+                    'appointments',
+                    newAppointment.id,
+                ])
+            },
+            ...options,
+        }
+    )
+}
+
+function useAppointmentDelete(id: NanoId, options?: MutationOptions) {
+    const queryClient = useQueryClient()
+
+    return useMutation(() => api.delete(`/appointments/${id}`), {
+        onSuccess: () => {
+            // Обновляем данные после успешного удаления объекта
+            queryClient.invalidateQueries(['appointments', 'all'])
+        },
+        ...options,
+    })
+}
+
+export {
+    useAppointments,
+    useAppointment,
+    useAppointmentCreate,
+    useAppointmentUpdate,
+    useAppointmentDelete,
+}
