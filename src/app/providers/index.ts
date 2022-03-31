bfb7235ec0568502e3b3f3d4c9aebf1dd96e2be2
Evgeniy Talko <etalko@gmail.com>
2022-03-31 17:32:33 +0300

    refactor: move reac-query app setup to HOC

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -2,4 +2,6 @@ import { flow } from 'fp-ts/function'
 
 import { withRouter } from 'app/providers/with-router'
 
-export const withProviders = flow(withRouter)
+import { withReactQuery } from './with-react-query'
+
+export const withProviders = flow(withRouter, withReactQuery)
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import { QueryClientProvider, QueryClient } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { worker } from 'mocks/server'
 
 import App from 'app'
 
-const client = new QueryClient()
 worker.start()
 
-ReactDOM.render(
-    <React.StrictMode>
-        <QueryClientProvider client={client}>
-            <ReactQueryDevtools initialIsOpen={false} />
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
